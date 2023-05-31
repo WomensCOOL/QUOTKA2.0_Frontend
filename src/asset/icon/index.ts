2a3c9820c86a87b9812beb92372619bb38d713de
[talkingOrange] <sonrisa-bonita@naver.com>
2023-05-31 09:26:08 +0900

    [ feat ] icon 추가

---
+++ src/asset/icon/index.ts
@@ -0,0 +1,4 @@
+export { ReactComponent as HomeIc } from "./homeIc.svg";
+export { ReactComponent as UserIc } from "./userIc.svg";
+export { ReactComponent as LogoIc } from "./logoIc.svg";
+export { ReactComponent as PencilIc } from "./pencilIc.svg";
