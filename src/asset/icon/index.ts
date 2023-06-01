dbc9fdd834ba52e3f9ef6384823ff92d9470f264
[talkingOrange] <sonrisa-bonita@naver.com>
2023-06-01 11:51:22 +0900

    [ etc ] icon 추가

--- src/asset/icon/index.ts
+++ src/asset/icon/index.ts
@@ -1,4 +1,12 @@
+/** Header */
 export { ReactComponent as HomeIc } from "./homeIc.svg";
 export { ReactComponent as UserIc } from "./userIc.svg";
 export { ReactComponent as LogoIc } from "./logoIc.svg";
 export { ReactComponent as PencilIc } from "./pencilIc.svg";
+
+/** Quote */
+export { ReactComponent as ExamIc } from "./examIc.svg";
+export { ReactComponent as CommentIc } from "./commentIc.svg";
+export { ReactComponent as HeartIc } from "./heartIc.svg";
+export { ReactComponent as ShareIc } from "./shareIc.svg";
+export { ReactComponent as StarIc } from "./starIc.svg";
