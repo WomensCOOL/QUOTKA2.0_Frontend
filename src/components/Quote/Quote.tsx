e906c4f5e6b5f997bbe9d58b6cb08c749b9ca9cf
[talkingOrange] <sonrisa-bonita@naver.com>
2023-06-02 21:28:09 +0900

    [ feat ] 프로필 scroll 이벤트 추가

--- src/components/Quote/Quote.tsx
+++ src/components/Quote/Quote.tsx
@@ -6,18 +6,69 @@ import {
   StarIc,
 } from "../../asset/icon/index";
 
+import React, { useEffect,useState, useRef } from 'react';
 import styled from "styled-components";
 import tw from "twin.macro";
 
 import { Link } from "react-router-dom";
 
 export default function Quote() {
+/*   const componentRef = useRef<HTMLDivElement>(null);
+  const [scrollPosition, setScrollPosition] = useState<number>(0);
+  function handleScroll() {
+    const currentPosition = window.scrollY;
+    setScrollPosition(currentPosition);
+  };
+
+  window.addEventListener('scroll', handleScroll);
+
+  useEffect(() => {
+    const element = componentRef.current;
+    const rect = element?.getBoundingClientRect();
+    console.log( rect?.y);
+    console.log(element);
+   
+  }, [Element, scrollPosition]);
+   */
+
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const [scrollPosition, setScrollPosition] = useState<number>(0);
+  const [initialOffset, setInitialOffset] = useState<number>(0);
+
+  function handleScroll() {
+    const currentPosition = window.scrollY;
+    setScrollPosition(currentPosition);
+  }
+
+  useEffect(() => {
+    const wrapperElement = wrapperRef.current;
+    const wrapperRect = wrapperElement?.getBoundingClientRect();
+    if (wrapperRect && wrapperElement) {
+      if (initialOffset === 0) {
+        setInitialOffset(wrapperRect.top);
+      }
+      const yOffset = scrollPosition - initialOffset;
+      wrapperElement.style.transform = `translateY(${scrollPosition}px)`;
+     
+    }
+  }, [scrollPosition, initialOffset, Element]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+
   return (
     <QuoteWrapper>
       <QuoteContainer>
+      <div ref={wrapperRef} style={{ position: 'relative', transition: 'transform 0.3s' }}>
         <UserWrapper>
           <ExamIc />
         </UserWrapper>
+        </div>
         <TextWrapper>
           <UserLink to="/user">얏호</UserLink>
           <FollowBtn>팔로우</FollowBtn>
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -4,6 +4,7 @@ export default function Main() {
   return (
     <div>
       <Quote />
+      <Quote />
     </div>
   );
 }
