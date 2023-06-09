64d4fdb9e091e94438cb7e3e651726f6cfed3370
[talkingOrange] <sonrisa-bonita@naver.com>
2023-06-09 21:42:05 +0900

    [ feat ] trending item ui

--- src/components/Main/Trending.tsx
+++ src/components/Main/Trending.tsx
@@ -2,35 +2,44 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
 import "swiper/css/navigation";
+import styled from "styled-components";
+import tw from "twin.macro";
 
 import { Navigation } from "swiper";
 import TrendingItem from "./TrendingItem";
 
 export default function Trending() {
   return (
-    <>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        <SwiperSlide>
-          <div className="text-gray-600 container px-5 py-12 mx-auto flex flex-wrap -m-4">
-            <div className="flex flex-wrap -m-4">
-              <TrendingItem rank={1} />
-              <TrendingItem rank={2} />
-              <TrendingItem rank={3} />
-              <TrendingItem rank={4} />
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="text-gray-600 body-font container px-5 py-12 mx-auto flex flex-wrap -m-4">
-            <div className="flex flex-wrap -m-4">
-              <TrendingItem rank={5} />
-              <TrendingItem rank={6} />
-              <TrendingItem rank={7} />
-              <TrendingItem rank={8} />
-            </div>
-          </div>
-        </SwiperSlide>
-      </Swiper>
-    </>
+    <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+      <SwiperSlide>
+        <TrendingWrapper>
+          <TrendingBox>
+            <TrendingItem rank={1} />
+            <TrendingItem rank={2} />
+            <TrendingItem rank={3} />
+            <TrendingItem rank={4} />
+          </TrendingBox>
+        </TrendingWrapper>
+      </SwiperSlide>
+      <SwiperSlide>
+      <TrendingWrapper>
+          <TrendingBox>
+            <TrendingItem rank={5} />
+            <TrendingItem rank={6} />
+            <TrendingItem rank={7} />
+            <TrendingItem rank={8} />
+          </TrendingBox>
+        </TrendingWrapper>
+      </SwiperSlide>
+    </Swiper>
   );
 }
+
+const TrendingWrapper = styled.div`
+  ${tw`
+text-gray-600 px-5 py-12 mx-auto flex flex-wrap m-6`}
+`;
+
+const TrendingBox = styled.div`
+  ${tw`flex flex-wrap -m-4`}
+`;
