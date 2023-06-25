bba89710004f98de849f1fbe54f4388e4cbc2306
[talkingOrange] <sonrisa-bonita@naver.com>
2023-06-25 17:12:43 +0900

    [ style ] trending style 끝

--- src/components/Main/Trending.tsx
+++ src/components/Main/Trending.tsx
@@ -10,36 +10,60 @@ import TrendingItem from "./TrendingItem";
 
 export default function Trending() {
   return (
-    <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-      <SwiperSlide>
-        <TrendingWrapper>
-          <TrendingBox>
-            <TrendingItem rank={1} />
-            <TrendingItem rank={2} />
-            <TrendingItem rank={3} />
-            <TrendingItem rank={4} />
-          </TrendingBox>
-        </TrendingWrapper>
-      </SwiperSlide>
-      <SwiperSlide>
-      <TrendingWrapper>
-          <TrendingBox>
-            <TrendingItem rank={5} />
-            <TrendingItem rank={6} />
-            <TrendingItem rank={7} />
-            <TrendingItem rank={8} />
-          </TrendingBox>
-        </TrendingWrapper>
-      </SwiperSlide>
-    </Swiper>
+    <SwiperWrapper>
+      <Swiper
+        navigation={{
+          prevEl: ".swiper-button-prev",
+          nextEl: ".swiper-button-next",
+        }}
+        modules={[Navigation]}
+        className="mySwiper "
+      >
+        <div className="swiper-button-prev" />
+        <div className="swiper-button-next" />
+        <SwiperSlide>
+          <TrendingWrapper>
+            <TrendingBox>
+              <TrendingItem rank={1} />
+              <TrendingItem rank={2} />
+              <TrendingItem rank={3} />
+              <TrendingItem rank={4} />
+            </TrendingBox>
+          </TrendingWrapper>
+        </SwiperSlide>
+        <SwiperSlide>
+          <TrendingWrapper>
+            <TrendingBox>
+              <TrendingItem rank={5} />
+              <TrendingItem rank={6} />
+              <TrendingItem rank={7} />
+              <TrendingItem rank={8} />
+            </TrendingBox>
+          </TrendingWrapper>
+        </SwiperSlide>
+      </Swiper>
+    </SwiperWrapper>
   );
 }
 
+const SwiperWrapper = styled.div`
+  ${tw`
+     text-white pl-16 pr-16 pt-6 max-w-3xl cursor-pointer`}
+
+  .swiper-button-prev {
+    color: white;
+  }
+
+  .swiper-button-next {
+    color: white;
+  }
+`;
+
 const TrendingWrapper = styled.div`
   ${tw`
-text-gray-600 px-5 py-12 mx-auto flex flex-wrap m-6`}
+text-gray-600 px-5 py-4 mx-auto flex flex-wrap `}
 `;
 
 const TrendingBox = styled.div`
-  ${tw`flex flex-wrap -m-4`}
+  ${tw`flex flex-wrap -m-4 `}
 `;
--- src/components/Main/TrendingItem.tsx
+++ src/components/Main/TrendingItem.tsx
@@ -1,22 +1,22 @@
 export default function TrendingItem({ rank }: { rank: number }) {
   return (
     <>
-      <div className=" sm:w-1/2 p-4">
-        <div className="flex relative">
-          <div className="px-8 py-10 absolute inset-0 w-full h-full object-cover object-center bg-white">
-            <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+      <div className=" sm:w-1/2 pl-1 pr-1 pb-2">
+        <div className="flex relative h-[6rem]">
+          <div className="px-7 py-3 absolute inset-0 w-full h-full object-cover object-center bg-[#FCD34D] bg-opacity-40 rounded-sm">
+            <h2 className="tracking-widest text-sm title-font font-medium text-white">
               {rank}
             </h2>
-            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+            <h1 className="title-font text-lg font-medium text-white">
               고추장 냠냠 명언 글귀 냠냠
             </h1>
-            <p className="leading-relaxed">#어디서 #들었는지</p>
+            <p className="leading-relaxed text-gray-400">#어디서 #들었는지</p>
           </div>
-          <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
-            <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">
+          <div className="px-7 py-3 relative z-10 w-full bg-[#212121] opacity-0 hover:opacity-100 rounded-sm">
+            <h2 className="tracking-widest text-sm title-font font-medium text-gray-100 ">
               {rank}
             </h2>
-            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+            <h1 className="title-font text-lg font-medium text-white">
               글쓴이
             </h1>
             <p className="leading-relaxed">뭐 팔로우 수나 이런거 보여줄까</p>
