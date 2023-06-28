0b9b4e096e7521df1568e6e8dcfa8851bdde7b29
[talkingOrange] <sonrisa-bonita@naver.com>
2023-06-28 10:52:43 +0900

    [ feat ] 팝업 UI 및 띄우기

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,19 @@
 import Trending from "../components/Main/Trending";
 import Quote from "../components/Quote/Quote";
+import Write from "./Write";
+import { useRecoilState } from "recoil";
+import { showPopup } from "../recoil/showPopup";
 
 export default function Main() {
+  const [show, setShow] = useRecoilState(showPopup);
+  function hideShow() {
+    setShow(!show);
+  }
   return (
-    <div>
+    <div onClick={hideShow}>
       <Trending />
       <Quote />
+      {show && <Write />}
     </div>
   );
 }
--- src/pages/Write.tsx
+++ src/pages/Write.tsx
@@ -1,8 +1,10 @@
+import Popup from "../components/Write/Popup";
 
 export default function WritePage() {
   return (
     <div>
-    
+      <Popup />
     </div>
   );
 }
+
